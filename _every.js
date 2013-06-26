e3bba2899843ea43182c3e8c0d14e079fcdc03c6
rasmusbp <rasmusbangsted@gmail.com>
2013-06-26 15:59:15 +0200

    Update _every.js

--- _every.js
+++ _every.js
@@ -11,7 +11,6 @@
   * 
   * @author Rasmus Bangsted Pedersen
   */
- (function () {
  Handlebars.registerHelper('every', function(array, options, elseFn) {
 
     if (array && array.length > 0) {
@@ -88,4 +87,3 @@
         return false;
     }
 });
-})();
